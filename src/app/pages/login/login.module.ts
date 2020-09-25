83de858c990bd18e6519ff8296234924552a09ab
luiscolungaperez <perez-luis.colunga@gmail.com>
2020-09-25 12:50:45 -0500

    [ADD] Add ReactiveFormsModule to login and register

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,12 +1,13 @@
 import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, SharedModule],
+  imports: [CommonModule, LoginRoutingModule, SharedModule, FormsModule, ReactiveFormsModule],
 })
 export class LoginModule {}
--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -2,11 +2,12 @@ import { SharedModule } from './../../shared/shared.module';
 import { RegisterComponent } from './components/register/register.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { RegisterRoutingModule } from './register-routing.module';
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, RegisterRoutingModule, SharedModule],
+  imports: [CommonModule, RegisterRoutingModule, SharedModule, FormsModule, ReactiveFormsModule],
 })
 export class RegisterModule {}
