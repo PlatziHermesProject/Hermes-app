daa8604fae7eb527919b1a5175aa38d80eda2124
frxncismor <frxncismor@gmail.com>
2020-09-25 22:17:17 -0500

    [ADD] feel-bad design

--- src/app/pages/feel-bad/components/feel-bad/feel-bad.component.ts
+++ src/app/pages/feel-bad/components/feel-bad/feel-bad.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-feel-bad',
   templateUrl: './feel-bad.component.html',
-  styleUrls: ['./feel-bad.component.scss']
+  styleUrls: ['./feel-bad.component.scss'],
 })
 export class FeelBadComponent implements OnInit {
+  organizations = [
+    {
+      name: 'Organization',
+      phone: '333-333-333',
+      country: 'Mexico',
+      image: 'assets/images/salud_mental.jpg',
+    },
+    {
+      name: 'Organization',
+      phone: '333-333-333',
+      country: 'Colombia',
+      image: 'assets/images/terapia.jpg',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
