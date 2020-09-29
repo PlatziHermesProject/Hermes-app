e5cfac9e597d1edb3da010075228458918c19fc2
luiscolungaperez <perez-luis.colunga@gmail.com>
2020-09-29 01:21:23 -0500

    [ADD] query graphql

--- src/app/pages/inbox/components/inbox/inbox.component.ts
+++ src/app/pages/inbox/components/inbox/inbox.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-inbox',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inbox.component.scss']
 })
 export class InboxComponent implements OnInit {
+  hello: any;
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          query hello{
+            hello
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.hello = result.data && result.data.hello;
+      });
   }
-
 }
