55d2e38df3d855344395bfcbc67970754a6d4306
frxncismor <frxncismor@gmail.com>
2020-10-04 17:32:19 -0500

    [FIX] snackbar on write page

--- src/app/pages/write/components/write/write.component.ts
+++ src/app/pages/write/components/write/write.component.ts
@@ -2,27 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { WriteService } from './../../../../core/services/write/write.service';
 import { UserService } from './../../../../core/services/user/user.service';
-import {
-  MatSnackBar,
-  MatSnackBarHorizontalPosition,
-  MatSnackBarVerticalPosition,
-} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-write',
   templateUrl: './write.component.html',
-  styleUrls: ['./write.component.scss']
+  styleUrls: ['./write.component.scss'],
 })
 export class WriteComponent implements OnInit {
-  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
-  verticalPosition: MatSnackBarVerticalPosition = 'top';
   form: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
     private writeService: WriteService,
-    private _snackBar: MatSnackBar,
     private userService: UserService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.buildForm();
@@ -30,26 +22,26 @@ export class WriteComponent implements OnInit {
   }
 
   createLetter(event: Event): void {
-    const user = "7";
-    const name = "Yasare";
+    const user = '7';
+    const name = 'Yasare';
     const content = this.textField.value;
     event.preventDefault();
-    this.writeService.addLetter(user, name, content)
-      .subscribe(({ data: { createLetter }}) => {
+    this.writeService
+      .addLetter(user, name, content)
+      .subscribe(({ data: { createLetter } }) => {
         this.textField.reset();
         this.openSnackBar(createLetter.message);
-      })
+      });
   }
 
-  viewUser(): any{
-    const user = this.userService.getValueTokenKey("user_id");
-    const email = this.userService.getValueTokenKey("email");
-    
+  viewUser(): any {
+    const user = this.userService.getValueTokenKey('user_id');
+    const email = this.userService.getValueTokenKey('email');
   }
 
   buildForm(): void {
     this.form = this.formBuilder.group({
-      text: ['', [Validators.required]]
+      text: ['', [Validators.required]],
     });
   }
   get textField(): string | any {
@@ -57,11 +49,12 @@ export class WriteComponent implements OnInit {
   }
 
   openSnackBar(message: string): any {
-    this._snackBar.open(message, 'x', {
-      duration: 3000,
-      horizontalPosition: this.horizontalPosition,
-      verticalPosition: this.verticalPosition,
-    });
+    console.log({ message });
+    const snackbar = document.getElementById('snackbar');
+    snackbar.innerHTML = message;
+    snackbar.className = 'show';
+    setTimeout(() => {
+      snackbar.className = snackbar.className.replace('show', '');
+    }, 3000);
   }
-
 }
